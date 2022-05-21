b0cba3c31f3aa25e37e529a53529e65c63be498c
hp <hp@DESKTOP-N3HB74L.bbrouter>
2022-05-21 15:54:30 +0530

    First Application

---
+++ src/com/resource/ContainerEx.java
@@ -0,0 +1,10 @@
+package com.resource;
+
+public class ContainerEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Exporting into GitHub");
+	}
+
+}
